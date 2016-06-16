2d78367e97020b379ad4ad2f1a17dde9db1b5e73
Beir Bear <beir.bear@gmail.com>
2016-06-16 16:14:00 +0200

    Initial Test

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 setup(name='harmonicIO',
       version='0.1.0',
       install_requires=['falcon', 'urllib3'],
-      packages=['data_source'],
+      packages=['data_source', 'master', 'worker'],
       entry_points={
           'console_scripts': [
               'data_source = data_source.__main__:main',
               'master = master.__main__:main',
+              'worker = worker.__main__:main',
           ]
       }
       )
