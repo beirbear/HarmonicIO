33f8ca40abc1358341ba940b2b85c51511cfa081
Beir Bear <beir.bear@gmail.com>
2016-06-20 20:29:57 +0200

    Generallity

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 setup(name='harmonicIO',
       version='0.1.0',
       install_requires=['falcon', 'urllib3'],
-      packages=['data_source', 'master', 'worker'],
+      packages=['data_source', 'master', 'worker', 'data_repository'],
       entry_points={
           'console_scripts': [
               'data_source = data_source.__main__:main',
               'master = master.__main__:main',
               'worker = worker.__main__:main',
+              'data_repository = data_repository.__main__:main'
           ]
       }
       )
