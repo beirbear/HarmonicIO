0a85e183db6643dd470bc070643f87e3249798a8
beirbear <beir.bear@gmail.com>
2017-06-29 10:18:10 +0700

    Container integration (manual flow)

--- install_dependency.sh
+++ install_dependency.sh
@@ -16,9 +16,21 @@ sudo apt-get install python3.5
 echo "Install setup tool"
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3.5
 
+# Install Colors
+echo "Install Color"
+git clone https://github.com/verigak/colors.git
+cd color
+sudo python3.5 setup.py install
+
 # Install REST framework (Falcon)
 echo "Install REST framework (Falcon)"
 git clone https://github.com/falconry/falcon.git
 # install on python 3.5 (The one that you just has installed)
 cd falcon
+sudo python3.5 setup.py install
+
+echo "Install docker-py library"
+echo "Install docker-py"
+git clone https://github.com/docker/docker-py.git
+cd docker-py
 sudo python3.5 setup.py install
